2b94a983359bb5d6899e1b35b2075a1b868abe97
James-hub-ctrl <rodesjames128@gmail.com>
2025-08-18 11:00:44 +0100

    Add files via upload

--- calculator2.js
+++ calculator2.js
@@ -4,19 +4,14 @@ const reciprocalEl = document.getElementById('reciprocal')
 const percentageEl = document.getElementById('percentage')
 const inverseEl = document.getElementById('inverse')
 const squareRootEl = document.getElementById('squareRoot')
-const del = document.getElementById('del')
-const factorialEl = document.getElementById('factorial')
-
-console.log(del)
-
+const del = document.getElementById('delT')
+const factorialEl = document.getElementById('fac')
 function appendValue(value) {
   inputEL.value += value
 }
-
 function clearDisplay() {
   inputEL.value = ''
 }
-
 function calculateResult() {
   try {
     inputEL.value = eval(inputEL.value)
@@ -26,7 +21,6 @@ function calculateResult() {
 }
 
 // CUSTOM FOR EXTRA OPERATIONS
-
 squareEl.addEventListener('click', function () {
   inputEL.value = Math.pow(parseFloat(inputEL.value), 2)
 })
@@ -35,11 +29,9 @@ reciprocalEl.addEventListener('click', function () {
   const x = parseFloat(inputEL.value)
   inputEL.value = x !== 0 ? 1 / x : 'error'
 })
-
 percentageEl.addEventListener('click', function () {
   inputEL.value = parseFloat(inputEL.value) / 100
 })
-
 inverseEl.addEventListener('click', function () {
   const x = parseFloat(inputEL.value)
   inputEL.value = x !== 0 ? 1 / (x * x) : 'error'
@@ -49,22 +41,25 @@ squareRootEl.addEventListener('click', function () {
   inputEL.value = Math.sqrt(parseFloat(inputEL.value))
 })
 
-deleteEl.addEventListener('click', function () {
-  inputEL.value = inputEL.value.slice(0, -1)
+del.addEventListener('click', function () {
+  console.log('the button')
+  // inputEL.value = inputEL.value.slice(0, -1)
 })
 
 factorialEl.addEventListener('click', function () {
-  let x = parseFloat(inputEL.value)
-  if (x < 0 || Number.isInteger(x)) {
-    inputEL.value = 'error'
+  console.log('the button clicked')
+  // =======-----
+  //   let x = parseFloat(inputEL.value)
+  //   if (x < 0 || Number.isInteger(x)) {
+  //     inputEL.value = 'error'
 
-    return
-  }
+  //     return
+  //   }
 
-  let result = 1
-  for (let i = 2; i <= x; i++) {
-    result *= i
-  }
+  //   let result = 1
+  //   for (let i = 2; i <= x; i++) {
+  //     result *= i
+  //   }
 
-  inputEL.value = result
+  //   inputEL.value = result
 })
